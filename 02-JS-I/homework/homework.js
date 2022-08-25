956523fc11f3cd09c09b609d2fa94376836ee461
unknown <alexcba38@gmail.com>
2022-08-24 21:33:45 -0300

    homework incomplet

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return (str)
   
 }
 
@@ -35,6 +36,9 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  suma = x + y;
+  return suma;
+
   
 }
 
